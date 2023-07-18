06afcc8660052d2822d5b7cd1fa2cc2664e6bf65
Somto Ufondu <somtoufondu@gmail.com>
2023-07-18 13:42:02 -0600

    Added PythonKit package which still did not solve the problem.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
+import PythonKit
 
 let package = Package(
     name: "Hours Tracker",
